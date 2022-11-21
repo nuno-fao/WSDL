4d658fff174528a481a4c7d936f72c32080a660e
Nunation <oliveiranunopt@gmail.com>
2022-11-21 19:10:08 +0000

    flask basic site

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = "key_super_secreta_não_digam_a_ninguém"
+
+@app.route("/")  # this sets the route to this page
+def index():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
