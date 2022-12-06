0a9906e462ebb34671e88c52d4a4863967b58647
rocas777 <lmiguelapinto@gmail.com>
2022-12-06 10:49:24 +0000

    removed idea stuff

--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ from flask import Flask, redirect, url_for, render_template, request, session
 app = Flask(__name__)
 app.secret_key = "key_super_secreta_não_digam_a_ninguém"
 
+
 @app.route("/")  # this sets the route to this page
 def index():
-	return render_template('index.html')
+    args = request.args.to_dict()
+    data_filter = "Any"
+    if "data-query" in args:
+        data_filter = args["data-query"]
+    return render_template('index.html', data_filter=data_filter)
 
 
 @app.route("/results")
 def results():
-      
-      return render_template('results.html')
+    return render_template('results.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
