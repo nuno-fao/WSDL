8083a8e73c68ea2845de207fe513b4a11410b992
Nunation <oliveiranunopt@gmail.com>
2022-12-11 23:22:10 +0000

    club page

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import math
 from flask import Flask, render_template, request
 from owlready2 import *
 from urllib.parse import unquote_plus
@@ -96,18 +97,21 @@ def toJson(entitiesList):
 def clup(name):
     name = unquote_plus(name)
 
-    get_ontology("../rdf/result").load()
-    q = """
-    select distinct ?club ?ar ?v where {
-      ?club  a <http://www.semanticweb.org/miguel/ontologies/2022/10/FootyPedia#Club> .
-      ?club  ?r "%s" 
-      ?club  ?ar ?v
-    }
-    """ % name
+    # get_ontology("../rdf/result").load()
+    # q = """
+    # select distinct ?club ?ar ?v where {
+    #   ?club  a <http://www.semanticweb.org/miguel/ontologies/2022/10/FootyPedia#Club> .
+    #   ?club  ?r "%s" 
+    #   ?club  ?ar ?v
+    # }
+    # """ % name
 
-    club = list(default_world.sparql(q))
-    club = toJson(club)
-    return render_template('club.html', id=id, club=club)
+    # club = list(default_world.sparql(q))
+    # club = toJson(club)
+
+    club= [{  "GK": [    [25, "Zaidu Sanusi"], [25, "Zaidu Sanusi"],[25, "Zaidu Sanusi"],[25, "Zaidu Sanusi"],[25, "Zaidu Sanusi"],   [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"]  ],  "DEF": [    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"]  ],  "MID": [    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"]  ],  "ATTACK": [    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"],    [25, "Zaidu Sanusi"]  ]},{  "Liga dos Campe\u00f5es": ["6", "4", "0", "2", "12-7"],  "Liga Portuguesa": ["13", "9", "2", "2", "31-9"],  "Superta\u00e7a": ["1", "1", "0", "0", "3-0"],  "Ta\u00e7a de Portugal": ["2", "2", "0", "0", "9-0"],  "Ta\u00e7a da Liga": ["2", "1", "1", "0", "4-2"],  "": ["24", "17", "3", "4", "59-18"]},{  "Nome": ["Futebol Clube do Porto"],  "Alcunhas": ["Drag\u00f5es, Azuis e Brancos, Portistas"],  "Associa\u00e7\u00e3o": ["AF Porto"],  "Presidente": ["Jorge Nuno de Lima Pinto da Costa"],  "Evolu\u00e7\u00e3o hist\u00f3rica": ["FC Porto"],  "Morada": [    "Est\u00e1dio do Drag\u00e3o, Entrada Nascente, Porta 15, Piso 3 Via Futebol Clube do Porto, 4350-415 Porto"],    "Site Oficial": ["http://www.fcporto.pt"],    "E-mail": ["fcporto@fcporto.pt"],    "Rankings": ["16", "60"],    "Hino Oficial": [""],    "Ano de Funda\u00e7\u00e3o": ["1893-09-28"],    "Cidade": ["Porto"],    "Pa\u00eds": ["Portugal"],    "Marca Equipamento": ["New Balance"],    "Patroc\u00ednio": [      "Betano | New Balance | Revigr\u00e9s | Super Bock | MEO"    ],    "Equipamento": [""],    "Outras Liga\u00e7\u00f5es": ["efgi"],    "Num.FPF": ["529"]  },  {    "Ta\u00e7a Intercontinental": "2",    "Superta\u00e7a Europeia": "1",    "Liga dos Campe\u00f5es": "2",    "Europa League": "2",    "Liga Portuguesa": "30",    "Ta\u00e7a de Portugal": "18",    "Superta\u00e7a C\u00e2ndido de Oliveira": "23",    "Campeonato de Portugal (Extinto)": "4"  },  "https://www.zerozero.pt/img/logos/equipas/9_imgbank.png"]
+    club_name="FC Porto"
+    return render_template('club.html', club_name=club_name ,club_info=club)
         
 @app.route("/league/<id>_<season>")
 def league(id, season):
@@ -200,7 +204,10 @@ def player(id):
 
     return render_template('player.html', player=player_example)
 
+def ceiling(nr,div):
+    return math.ceil(nr/div)
 
+app.jinja_env.globals.update(ceiling=ceiling)
 
 if __name__ == "__main__":
     app.run(debug=True)
